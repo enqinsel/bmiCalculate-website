118fc44982c9e6ab041bf62cf1cc700e694d9725
enqinsel <enqinsel@gmail.com>
2022-12-21 21:43:35 +0300

    athleteMan component created

--- src/App.vue
+++ src/App.vue
@@ -1,56 +1,12 @@
 <script setup>
-import { onMounted, ref } from "vue";
 import NavComp from "./components/Navbar/NavComp.vue";
 import CalculateComp from "./components/Calculate/CalculateComp.vue";
-
-
-const rightItem = ref();
-
-onMounted(() => {
-  setInterval(function () {
-    rightItem.value += -500;
-    if ((rightItem.value = -1500)) {
-      clearInterval();
-    }
-  }, 100);
-});
+import AthleteMan from "./components/AthleteMan/AthleteMan.vue";
 </script>
 
 <template>
-  <img
-    :style="`transform: translateX(${rightItem}px)`"
-    class="ath"
-    src="./assets/athleteman.svg"
-  />
-  <img
-    :style="`transform: translateX(${rightItem}px)`"
-    class="bgc"
-    src="./assets/athletemanBackground.svg"
-  />
-
+  <AthleteMan></AthleteMan>
   <NavComp></NavComp>
   <CalculateComp></CalculateComp>
 </template>
 
-<style scoped>
-.ath {
-  position: absolute;
-  top: 50px;
-  left: 2200px;
-  transition: all 1s ease;
-  z-index: 1;
-}
-.bgc {
-  position: absolute;
-  top: 60px;
-  left: 2450px;
-  transition: all 1s ease;
-  z-index: 0;
-}
-
-/* .calsculateComp {
-  z-index: -2;
-} */
-
-
-</style>
---
+++ src/components/AthleteMan/AthleteMan.vue
@@ -0,0 +1,44 @@
+<script setup>
+import { onMounted, ref } from "vue";
+
+const rightItem = ref();
+
+onMounted(() => {
+  setInterval(function () {
+    rightItem.value += -500;
+    if ((rightItem.value = -1500)) {
+      clearInterval();
+    }
+  }, 100);
+});
+</script>
+
+<template>
+  <img
+    :style="`transform: translateX(${rightItem}px)`"
+    class="ath"
+    src="../../assets/athleteman.svg"
+  />
+  <img
+    :style="`transform: translateX(${rightItem}px)`"
+    class="bgc"
+    src="../../assets/athletemanBackground.svg"
+  />
+</template>
+
+<style scoped>
+.ath {
+  position: absolute;
+  top: 50px;
+  left: 2200px;
+  transition: all 1s ease;
+  z-index: 1;
+}
+.bgc {
+  position: absolute;
+  top: 60px;
+  left: 2450px;
+  transition: all 1s ease;
+  z-index: 0;
+}
+</style>
